66a51390c53f9a6e99b6524b78fd0d98e54c56fc
Ashley Richardson <ash@ashri.me>
2018-12-17 15:06:38 +0800

    Add structure for Board object

--- src/main/java/me/ashri/experiments/tictactoe/entities/Board.java
+++ src/main/java/me/ashri/experiments/tictactoe/entities/Board.java
@@ -1,11 +1,56 @@
 package me.ashri.experiments.tictactoe.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Board implements Serializable {
 
+    private final char[] values;
+    private final Set<Integer> crosses;
+    private final Set<Integer> naughts;
+    private final Set<Integer> empty;
+
+    public Board(char[] values) {
+        this.values = values;
+        this.crosses = new HashSet<>(9);
+        this.naughts = new HashSet<>(9);
+        this.empty = new HashSet<>(9);
+        process(values);
+    }
+
+    private void process(char[] values) {
+        for (int i = 0; i < values.length; i++) {
+            char c = values[i];
+            switch (c) {
+                case 'x':
+                    crosses.add(i);
+                    break;
+                case 'o':
+                    naughts.add(i);
+                    break;
+                default:
+                    empty.add(i);
+                    break;
+            }
+        }
+    }
+
+    public int getCrossCount() {
+        return crosses.size();
+    }
+
+    public int getNaughtCount() {
+        return naughts.size();
+    }
+
+    public int getEmptyCount() {
+        return empty.size();
+    }
+
     @Override
     public String toString() {
-        return "xxxxxxxxx";
+        return new String(values);
     }
+
 }
