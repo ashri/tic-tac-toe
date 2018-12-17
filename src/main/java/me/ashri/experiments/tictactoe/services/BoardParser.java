0a52bedd8c1c6d93e317bf862a5253dc535f5fab
Ashley Richardson <ash@ashri.me>
2018-12-17 15:06:54 +0800

    Complete board parsing structure which builds board and validates it

--- src/main/java/me/ashri/experiments/tictactoe/services/BoardParser.java
+++ src/main/java/me/ashri/experiments/tictactoe/services/BoardParser.java
@@ -9,16 +9,39 @@ import me.ashri.experiments.tictactoe.entities.BoardValidationException;
 public class BoardParser {
 
     public Board parse(String boardInput) {
+        if (boardInput != null) {
+            boardInput = boardInput.toLowerCase();
+        }
         validateInput(boardInput);
 
-        return new Board();
+        char[] input = boardInput.toCharArray();
+
+        Board board = new Board(input);
+
+        validateLogic(board);
+
+        return board;
     }
 
     private void validateInput(String boardInput) {
         if (boardInput == null || !boardInput.matches("[xo ]{9}")) {
             throw BoardValidationException.patternMismatch();
         }
+    }
 
+    private void validateLogic(Board board) {
+        if (board.getEmptyCount() == 0) {
+            throw BoardValidationException.logicError("board already full");
+        }
+        int crossCount = board.getCrossCount();
+        int naughtCount = board.getNaughtCount();
+
+        if (naughtCount > crossCount) {
+            throw BoardValidationException.logicError("too many \"o\"");
+        }
+        if (crossCount > (naughtCount + 1)) {
+            throw BoardValidationException.logicError("too many \"x\"");
+        }
     }
 
 }
